8d26ef87dbf4141bf4a68e33ea2f33222f901f1e
VikasRana007666 <cookthewebpage@gmail.com>
2023-04-05 12:53:50 +0530

    tailwind and boostrap font sizing setup correcttly

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Vite + React</h1>
+      <h1 className="underline">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
