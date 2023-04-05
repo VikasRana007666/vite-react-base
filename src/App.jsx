8fa7523a939d134dbd51dfb5ed2b043842ec01d9
VikasRana007666 <cookthewebpage@gmail.com>
2023-04-05 15:36:10 +0530

    some updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
-import { useState } from "react";
+import Home from "./pages/home";
 
-function App() {
-  const [count, setCount] = useState(0);
-  import.meta.env.TOKEN = "akjfdlkafd";
-
+export default function App() {
   return (
-    <div className="App">
-      <h1 className="underline">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <Home></Home>
     </div>
   );
 }
-
-export default App;
---
+++ src/pages/tasks.jsx
@@ -0,0 +1,41 @@
+export default function Tasks() {
+  const tasks = [
+    { id: 1, task: "Durward Reynolds", unavailable: false },
+    { id: 2, task: "Kenton Towne", unavailable: false },
+    { id: 3, task: "Therese Wunsch", unavailable: false },
+    { id: 4, task: "Benedict Kessler", unavailable: true },
+    { id: 5, task: "Katelyn Rohan", unavailable: false },
+  ];
+
+  return (
+    <div>
+      <div className="overflow-hidden bg-white shadow sm:rounded-lg">
+        <div className="px-4 py-5 sm:px-6 flex justify-between">
+          <h3 className="text-base font-semibold leading-6 text-gray-900">
+            Tasks
+          </h3>
+          <button className="px-3  bg-slate-800 text-white rounded-full">
+            Add
+          </button>
+        </div>
+        <div className="border-t border-gray-200">
+          <dl>
+            {tasks.map((item) => (
+              <div
+                key={item.id}
+                className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+              >
+                <dt className="text-sm font-medium text-gray-500">
+                  Task {item.id}
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                  {item.task}
+                </dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </div>
+  );
+}
